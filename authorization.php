26778db8abe2ab58fd9df217be953a01a45e463f
m.tutukov <m.tutukov@kokoc.tech>
2022-04-16 10:49:51 +0300

    авторизация

---
+++ authorization.php
@@ -0,0 +1,26 @@
+<?php
+// Подключение бд
+include_once("./config/init.php");
+
+// Подключение функций
+include_once("./functions/helpers.php");
+
+if (isset($_POST['password']) && isset($_POST['mail'])) {
+  $sql = "SELECT id, password FROM users WHERE mail = ?";
+  $pass = db_fetch_data($link, $sql, [$_POST['mail']]);
+
+  if (!isset($pass[0]['password'])) {
+    echo 'Не верный e-mail';
+    exit();
+  }
+
+  if (!password_verify($_POST['password'], $pass[0]['password'])) {
+    echo "Неверный пароль";
+    exit();
+  }
+
+  $_SESSION['user_id'] = $pass[0]['id'];
+} else {
+  echo 'Введите данные для входа';
+}
+exit();
--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Подключение бд
-include_once("./config/init.php");
-
-// Подключение функций
-include_once("./functions/helpers.php");
-
